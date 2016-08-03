3327ba3b77343ed2620af3bcce02c016fbc9d09c
gstylez <cgomez@Carloss-iMac.local>
2016-08-03 00:59:17 -0700

    adding updated files containing part 2 of assignment

--- homework-5/hw5.js
+++ homework-5/hw5.js
@@ -1,8 +1,22 @@
+if(window.name="Popup") {
+	window.onload = function() {
+		var btn = document.getElementById("btnAddEmail");
+		return false;
+		btn.onclick = fAddEmail();
+	}
+	function fAddEmail() {
+		alert()
+	}
+} else {
+	window.onload = function() {
+		var btn = document.getElementById("btnWindow");
+		return false;
+		btn.onclick = fOpenWin();
+	}
+	function fOpenWin() {
+		var x = screen.width/2 - 700/2;
+	    var y = screen.height/2 - 450/2;
+		window.open("hw5_dialog.html", "Popup", "width=500, height=200, top="+y+", left="+x);
+	}	
+}
 
-window.onload = function() {
-	var btn = document.getElementById("btnWindow");
-	btn.onclick = fOpenWin();
-}
-function fOpenWin() {
-	window.open("Popup", "width=500, height=200");
-}
