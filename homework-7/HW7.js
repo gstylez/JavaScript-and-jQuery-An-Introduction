b6e06b4096e882cd240f78d72923eea41213a1d0
gstylez <cgomez@Carloss-iMac.local>
2016-08-17 00:13:13 -0700

    adding new content

---
+++ homework-7/HW7.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+// 	btnSave.onclick = fSave;
+// 	btnCancel.onclick = fCancel;
+// 	btnHelp.onclick = fHelp;
+	divMain.onclick = fSaveCancelHelp;
+	
+});
+
+function fSave() {
+	alert("Data has been saved!");
+}
+function fCancel() {
+	alert("Operation has been canceled!");
+}
+function fHelp() {
+	alert("Help is not available!");
+}
+function fSaveCancelHelp(e) {
+	switch(e.target.id.toLowerCase()) {
+		 case n:
+	}
+}
+
+switch(expression) {
+    case n:
+        code block
+        break;
+    case n:
+        code block
+        break;
+    default:
+        default code block
+}
